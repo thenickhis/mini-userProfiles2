13688fed56f5e2682eebc80c26bdbeaec339fbdb
thenickhis <thenickhis@gmail.com>
2016-06-07 20:30:48 -0600

    push

--- controller.js
+++ controller.js
@@ -1,10 +1,9 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
-  $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
-
-
+	$scope.getUsers = function() {
+  		mainService.getusers().then(function(response) {
+  			$scope.users = response.data.data;
+  		});
+  	}
   $scope.getUsers();
-
 });
